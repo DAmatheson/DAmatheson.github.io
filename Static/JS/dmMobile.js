ee02ce18afcb3c6af35e63339ae236aa0a7efd21
DAmatheson <row.blood.eagle@gmail.com>
2014-04-28 19:55:45 -0400

    Trying to fix header issues

--- Static/JS/dmMobile.js
+++ Static/JS/dmMobile.js
@@ -116,6 +116,8 @@ function generic_Load(pageId, bottomMarginSelector)
     // Takes an option argument selector string. The matching elements get bottom margin added to them
     // Sets up form events and layout on load
     
+    $.mobile.ajaxEnabled = false; // Disable ajax loading
+    
     // Event that requires resizing the content panel
     $(document).on("pagecontainershow", pageId, sizeContentHeightToScreen);
 
