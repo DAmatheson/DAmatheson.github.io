fc3163ab0ccca9547f1a7994de807bf17f05d3b9
DAmatheson <row.blood.eagle@gmail.com>
2014-08-14 16:11:24 -0400

    Added filters required by last commit
    I edited the wrong files in the last commit. These are the dangers of
    having two copies of identical files.

--- ReadingList/js/filters.js
+++ ReadingList/js/filters.js
@@ -1,15 +1,4 @@
-/* filters.js
-    Purpose: Angular filters for BoardGameScorer
-
-    Revision History:
-        Drew Matheson, 2014.5.1: Created
-*/
-
-/// <reference path="angular/angular.js"/>
-
 'use strict';
-
-/* Filters */
 
 var rlFilters = angular.module('readList.filters', []);
 
@@ -22,6 +11,29 @@ rlFilters.filter('interpolate', ['version',
         };
     }
 ]);
+
+rlFilters.filter('prettyLink', 
+    function()
+    {
+        return function(text)
+        {
+            // Remove http//:, https://, and www. from the start of a Url
+            return String(text).replace(/^(http)?(s)?(:\/\/)?(www.)?/, "");
+        }
+    }
+);
+
+rlFilters.filter('absoluteLink', 
+    function()
+    {
+        return function(input)
+        {
+            // Adds http:// to the start of a url if it or https:// doesn't exist
+            return input.indexOf(/^http(s)?:\/\//) > -1 ? input : "http://" + input;
+        }
+    }
+);
+
 
 //<p>
 //    Showing of 'interpolate' filter:
